0145e716e2853d5f5faabb14f9c2a1e50e87de28
Jasmin1002 <jasmineolofin@gmail.com>
2022-10-22 14:35:40 +0100

    feat: Add application dir to manage views

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import jsonify
+
+# Bind function view to route /status
+@app_views.route('/status', strict_slashes=False)
+def api_status():
+    return jsonify({'status': 'OK'})
