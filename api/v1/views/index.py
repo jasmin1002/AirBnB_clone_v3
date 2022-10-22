94d44223e5b81e8d4af83ed687d6ebfee4563e1d
Jasmin1002 <jasmineolofin@gmail.com>
2022-10-22 14:52:03 +0100

    doc: Add documentation to detail each of code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+'''
+This script records operation later perform by
+blueprint when it extends flask application.
+'''
 
 from api.v1.views import app_views
 from flask import jsonify
 
+
 # Bind function view to route /status
 @app_views.route('/status', strict_slashes=False)
 def api_status():
+    '''Bind function view to endpoint /status'''
     return jsonify({'status': 'OK'})
