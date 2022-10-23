23a045189533e03061e5bef59c4bbeeaab4ad927
Jasmin1002 <jasmineolofin@gmail.com>
2022-10-23 01:13:55 +0100

    doc: Add documentation to blueprint base file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+'''
+Base blueprint config file
+'''
 
-#from api.v1.views.index import *
 from flask import Blueprint
 
 # application blueprint object
+#: app_views: Stores reference to blueprint instance obj
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# import operations specific to each route
+# or endpoint.
 from api.v1.views.index import *
+from api.v1.views.states import *
