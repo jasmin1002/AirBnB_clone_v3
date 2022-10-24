54138b6d0784a50acccb8bc063e7fda73b0cbfd6
Jasmin1002 <jasmineolofin@gmail.com>
2022-10-24 05:49:01 +0100

    feat: Add view func /city/<city_id>
    Add on a route /city/<city_id> to retrieve a
    specified city with a city ID.
    And include 404 handler for retrieving all cities.

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,7 +5,7 @@ to cities route.
 '''
 
 from api.v1.views import app_views
-from flask import json, make_response
+from flask import abort, make_response, json
 from models import storage
 from models.city import City
 from models.state import State
@@ -27,6 +27,8 @@ def fetch_cities(state_id):
     '''
     #: state (State instance): Keeps reference to State instance obj
     state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
     #: cities (list of dictionary): Stores all cities of a state
     cities = [city.to_dict() for city in state.cities]
     # Convert list to string
@@ -34,5 +36,26 @@ def fetch_cities(state_id):
     # Convert string to Response type
     response = make_response(response)
     # Set mime type to application/json
+    response.mimetype = 'application/json'
+    return response
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def fetch_city(city_id):
+    '''
+    Retrieve a city from city resources by city ID
+    Args:
+        city_id (str): argument parameter to city ID
+    Return:
+        Returns json city with 200 ok for success, otherwise error 404
+    '''
+    #: city (City instance): Keeps reference to City instance obj
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    # Convert dict_city to string
+    response = json.dumps(city.to_dict(), indent=2)
+    # convert string to Response type
+    response = make_response(response)
     response.mimetype = 'application/json'
     return response
